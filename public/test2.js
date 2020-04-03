4fc0a280c3f14095014832fb727c0c69520a82be
Sune Maute <sune-maute@web.de>
2020-04-03 14:07:24 +0200

    added arrows

--- public/test2.js
+++ public/test2.js
@@ -3,20 +3,20 @@ function handleChange(checkbox, id) {
   if(id.localeCompare('expend1')==0){
 
     if(checkbox.checked == true){
-      document.getElementById("hnwlbl_1").innerHTML = "Weniger lesen";
+      document.getElementById("hnwlbl_1").innerHTML = "&#8593;&nbsp;&nbsp;Weniger lesen&nbsp;&nbsp;&#8593;";
     }
     else{
-      document.getElementById("hnwlbl_1").innerHTML = "Mehr lesen";
+      document.getElementById("hnwlbl_1").innerHTML = "&#8595;&nbsp;&nbsp;Mehr lesen&nbsp;&nbsp;&#8595;";
     }
   }
 
   else {
 
     if(checkbox.checked == true){
-      document.getElementById("hnwlbl_2").innerHTML = "Weniger lesen";
+      document.getElementById("hnwlbl_2").innerHTML = "&#8593;&nbsp;&nbsp;Weniger lesen&nbsp;&nbsp;&#8593;";
     }
     else{
-      document.getElementById("hnwlbl_2").innerHTML = "Mehr lesen";
+      document.getElementById("hnwlbl_2").innerHTML = "&#8595;&nbsp;&nbsp;Mehr lesen&nbsp;&nbsp;&#8595;";
     }
 
   }
